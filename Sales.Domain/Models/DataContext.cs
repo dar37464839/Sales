1032ade7cc1101f38d2c5aaaaa4f255f52a7b8de
Dileisy <desarrollador8.sion@unac.edu.co>
2022-02-21 16:13:08 -0500

    Nueevos_Cambios

---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        //Cada que instancia una clase data o un objeto data context se conecta a default connection
+        public DataContext() : base("DefaultConnection"){
+
+        }
+    }
+}
