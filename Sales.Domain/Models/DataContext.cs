5607c9d242ea783ce9e71451b06a585205d9611b
Dileisy <desarrollador8.sion@unac.edu.co>
2022-07-23 20:42:50 -0500

    prueba cambios

--- Sales.Domain/Models/DataContext.cs
+++ Sales.Domain/Models/DataContext.cs
@@ -4,8 +4,11 @@
     public class DataContext : DbContext
     {
         //Cada que instancia una clase data o un objeto data context se conecta a default connection
-        public DataContext() : base("DefaultConnection"){
+        public DataContext() : base("ListInsc_Connection")
+        {
 
         }
+
+
     }
 }
